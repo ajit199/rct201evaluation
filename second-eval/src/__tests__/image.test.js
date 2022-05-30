6bcf6789776de118bc21493b01ef6bba685d438e
Ajit Singh <79194669+ajit199@users.noreply.github.com>
2022-05-30 16:57:48 +0530

    files removed

--- second-eval/src/__tests__/image.test.js
+++ second-eval/src/__tests__/image.test.js
@@ -4,7 +4,7 @@ import { Image } from "../Components/Image";
 describe("This test cases are for image component", () => {
   test("component should have alt", function () {
     render(<Image />);
-    let img = screen.findByAltText("userImage");
+    let img = screen.get;
     expect(img).toBeInTheComponent();
   });
 });
